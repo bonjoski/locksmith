b8df1867d03cec61792758ebe32de6591bf8706e
Bonjo <bonjoski@users.noreply.github.com>
2026-04-09 18:00:43 -0600

    feat: update Homebrew formula for v2.2.6

--- Formula/locksmith.rb
+++ Formula/locksmith.rb
@@ -9,14 +9,14 @@ class Locksmith < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bonjoski/locksmith/releases/download/v2.2.5/locksmith-darwin-arm64"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "a67f4fc4445c15ae8a4d52207c2d11af32bcb31b5a6cb19c24986cf60c919860"
 
       def install
         bin.install "locksmith-darwin-arm64" => "locksmith"
       end
     else
       url "https://github.com/bonjoski/locksmith/releases/download/v2.2.5/locksmith-darwin-amd64"
-      sha256 "REPLACE_WITH_AMD64_SHA256"
+      sha256 "8d1744e46ad8b185d82173515297279862995cf1a9488afc10197524bfd49fea"
 
       def install
         bin.install "locksmith-darwin-amd64" => "locksmith"
